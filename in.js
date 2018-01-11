f2f6be6667ed1b90a316dc55d9ca6d9cbf0a4bf0
Sose <sose94@gmail.com>
2018-01-11 10:42:50 +0100

    i guess that's it

--- in.js
+++ in.js
@@ -55,6 +55,10 @@ app.get('/productview', function(req, res)
 });
 
 
+//--------------------------------------------
+//Database stuff
+//-------------------------------------------
+
 //Read all
 app.get('/customer', function(req, res)
 {
@@ -95,9 +99,7 @@ app.post('/customer', function (req, res)
 
 		col.insertOne(req.body, function(err, result)
 		{
-			res.status(201);
 			res.json( {msg : 'Customer Created'} );
-			console.log(req.body);
 		});
 
 		db.close();
@@ -120,24 +122,20 @@ app.delete('/customer/:id', function(req, res)
 })
 
 
-// ===================
-// Products ===========
-// ====================
-// READ (all)
+// READ all
 app.get('/products', function (req, res) {
     MongoClient.connect(mongodburl, function (err, db) {
 
         var col = db.collection('product');
-        // Read All
+
         col.find().toArray(function (err, result) {
-            //res.setHeader('Access-Control-Allow-Origin', '*');
             res.json(result);
         });
         db.close();
     });
 });
 
-// READ (one)
+// READ one
 app.get('/products/:id', function (req, res) {
 
     MongoClient.connect(mongodburl, function (err, db) {
@@ -151,14 +149,14 @@ app.get('/products/:id', function (req, res) {
 });
 
 // CREATE
-app.post('/products/', function (req, res) {
+/*app.post('/products/', function (req, res) {
 
     MongoClient.connect(mongodburl, function (err, db) {
         var col = db.collection('product');
 
         col.insertOne(req.body, function (err, result) {
             //res.status(201);
-            res.json({ msg: 'Customer Created' });
+            res.json({ msg: 'products Created' });
         })
         db.close();
     });
@@ -171,44 +169,14 @@ app.delete('/products/:id', function (req, res) {
         var col = db.collection('product');
 
         col.deleteOne({ '_id': ObjectId(req.params.id) }, function (err, result) {
-            //res.status(204);
             res.json();
 
         });
 
         db.close();
     });
-});
+});*/
 
-
-// UPDATE
-app.put('/products/:id', function (req, res) {
-    
-        MongoClient.connect(mongodburl, function (err, db) {
-            var col = db.collection('product');
-    
-            col.updateOne({ '_id': ObjectId(req.params.id) }, {$set : req.body}, function(err, result){
-                //res.status(204);
-                res.json();
-            });
-            db.close();
-        });
-    });
-
-
-// UPDATE
-app.put('/customers/:id', function (req, res) {
-    
-        MongoClient.connect(mongodburl, function (err, db) {
-            var col = db.collection('customer');
-    
-            col.updateOne({ '_id': ObjectId(req.params.id) }, {$set : req.body}, function(err, result){
-                //res.status(204);
-                res.json();
-            });
-            db.close();
-        });
-    });
 
 //ORDERS
 
@@ -217,16 +185,14 @@ app.get('/orders', function (req, res) {
     MongoClient.connect(mongodburl, function (err, db) {
 
         var col = db.collection('order');
-        // Read All
         col.find().toArray(function (err, result) {
-            //res.setHeader('Access-Control-Allow-Origin', '*');
             res.json(result);
         });
         db.close();
     });
 });
 
-// READ (one)
+// Read one
 app.get('/orders/:id', function (req, res) {
 
     MongoClient.connect(mongodburl, function (err, db) {
@@ -246,43 +212,12 @@ app.post('/orders/', function (req, res) {
         var col = db.collection('order');
 
         col.insertOne(req.body, function (err, result) {
-            //res.status(201);
             res.json({ msg: 'Customer Created' });
         })
         db.close();
     });
 });
 
-// DELETE
-app.delete('/orders/:id', function (req, res) {
-
-    MongoClient.connect(mongodburl, function (err, db) {
-        var col = db.collection('order');
-
-        col.deleteOne({ '_id': ObjectId(req.params.id) }, function (err, result) {
-            //res.status(204);
-            res.json();
-
-        });
-
-        db.close();
-    });
-});
-
-
-// UPDATE
-app.put('/orders/:id', function (req, res) {
-    
-        MongoClient.connect(mongodburl, function (err, db) {
-            var col = db.collection('order');
-    
-            col.updateOne({ '_id': ObjectId(req.params.id) }, {$set : req.body}, function(err, result){
-                //res.status(204);
-                res.json();
-            });
-            db.close();
-        });
-    });
 
 
 app.listen(process.env.PORT || 3500);
